85e76e09e52037aa83677e321bb83df251374fb0
Michael McLean <m.mclean@outlook.com>
2026-04-28 18:51:58 -0500

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,11 +9,76 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# DX packages from Fedora repos - common to all versions
+FEDORA_PACKAGES=(
+    android-tools
+    bcc
+    bpftop
+    bpftrace
+    cockpit-bridge
+    cockpit-machines
+    cockpit-networkmanager
+    cockpit-ostree
+    cockpit-podman
+    cockpit-selinux
+    cockpit-storaged
+    cockpit-system
+    edk2-ovmf
+    flatpak-builder
+    incus
+    incus-agent
+    iotop
+    libvirt
+    libvirt-nss
+    lxc
+    nicstat
+    numactl
+    osbuild-selinux
+    p7zip
+    p7zip-plugins
+    podman-compose
+    podman-machine
+    podman-tui
+    qemu
+    qemu-char-spice
+    qemu-device-display-virtio-gpu
+    qemu-device-display-virtio-vga
+    qemu-device-usb-redirect
+    qemu-img
+    qemu-system-x86-core
+    qemu-user-binfmt
+    qemu-user-static
+    sysprof
+    trace-cmd
+    udica
+    virt-manager
+    virt-v2v
+    virt-viewer
+    ydotool
+)
+
+echo "Installing ${#FEDORA_PACKAGES[@]} DX packages from Fedora repos..."
+dnf5 -y install "${FEDORA_PACKAGES[@]}"
+
+CUSTOM_PACKAGES=(
+    onedrive
+    niri
+    noctalia-shell
+)
 
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
-dnf5 install -y niri noctalia-shell
+dnf5 -y install "${CUSTOM_PACKAGES[@]}"
+
+# Docker packages from their repo
+dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
+dnf -y install --enablerepo=docker-ce-stable \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-ce \
+    docker-ce-cli \
+    docker-compose-plugin \
+    docker-model-plugin
 
 # Use a COPR Example:
 #
@@ -25,3 +90,7 @@ dnf5 install -y niri noctalia-shell
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+systemctl enable swtpm-workaround.service
+systemctl enable ublue-os-libvirt-workarounds.service
+systemctl enable aurora-dx-groups.service
+systemctl enable --global aurora-dx-user-vscode.service
