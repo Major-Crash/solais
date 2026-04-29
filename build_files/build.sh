c4a9e11374292c3d065e1fc519f6bc1f1146f51c
Michael McLean <m.mclean@outlook.com>
2026-04-29 18:59:21 -0500

    add waydroid

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,76 +9,15 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# DX packages from Fedora repos - common to all versions
-FEDORA_PACKAGES=(
-    android-tools
-    bcc
-    bpftop
-    bpftrace
-    cockpit-bridge
-    cockpit-machines
-    cockpit-networkmanager
-    cockpit-ostree
-    cockpit-podman
-    cockpit-selinux
-    cockpit-storaged
-    cockpit-system
-    edk2-ovmf
-    flatpak-builder
-    incus
-    incus-agent
-    iotop
-    libvirt
-    libvirt-nss
-    lxc
-    nicstat
-    numactl
-    osbuild-selinux
-    p7zip
-    p7zip-plugins
-    podman-compose
-    podman-machine
-    podman-tui
-    qemu
-    qemu-char-spice
-    qemu-device-display-virtio-gpu
-    qemu-device-display-virtio-vga
-    qemu-device-usb-redirect
-    qemu-img
-    qemu-system-x86-core
-    qemu-user-binfmt
-    qemu-user-static
-    sysprof
-    trace-cmd
-    udica
-    virt-manager
-    virt-v2v
-    virt-viewer
-    ydotool
-)
-
-echo "Installing ${#FEDORA_PACKAGES[@]} DX packages from Fedora repos..."
-dnf5 -y install "${FEDORA_PACKAGES[@]}"
-
 CUSTOM_PACKAGES=(
     onedrive
     niri
     noctalia-shell
+    waydroid
 )
 
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 dnf5 -y install "${CUSTOM_PACKAGES[@]}"
-
-# Docker packages from their repo
-dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo --overwrite
-sed -i "s/enabled=.*/enabled=0/g" /etc/yum.repos.d/docker-ce.repo
-dnf -y install --enablerepo=docker-ce-stable \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-ce \
-    docker-ce-cli \
-    docker-compose-plugin \
-    docker-model-plugin
 
 # Use a COPR Example:
 #
@@ -89,8 +28,3 @@ dnf -y install --enablerepo=docker-ce-stable \
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
-systemctl enable swtpm-workaround.service
-systemctl enable ublue-os-libvirt-workarounds.service
-systemctl enable aurora-dx-groups.service
-systemctl enable --global aurora-dx-user-vscode.service
