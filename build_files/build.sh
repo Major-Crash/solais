73b0cc29e43a5f7d0737340970b91838d233369f
major-crash <majorcrash@thundermail.com>
2026-09-08 07:52:08 -0500

    Upgrade qt6-qtbase

--- build_files/build.sh
+++ build_files/build.sh
@@ -35,6 +35,7 @@ for repo in "${COPR_REPOS[@]}"; do
     dnf5 -y copr enable "$repo"
 done
 
+dnf5 -y upgrade qt6-qtbase.x86_64
 dnf5 -y install "${CUSTOM_PACKAGES[@]}"
 
 for repo in "${COPR_REPOS[@]}"; do
